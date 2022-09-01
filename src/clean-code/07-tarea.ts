e58a178a20b444b93a6e53a20c85847f62d0353b
Mauricio Cartagena <mc973220@gmail.com>
2022-09-01 16:27:01 -0400

    Section 4: Finisted

--- src/clean-code/07-tarea.ts
+++ src/clean-code/07-tarea.ts
@@ -1,50 +1,39 @@
-(()=>{
+(() => {
+  //* Aplicar el principio de responsabilidad única
+  //* Priorizar la composición frente a la herencia
 
-	//* Aplicar el principio de responsabilidad única
-	//* Priorizar la composición frente a la herencia
+  type HtmlType = "input" | "select" | "textarea" | "radio";
 
-	type HtmlType = 'input'|'select'|'textarea'|'radio';
- 
-	class HtmlElement {
-			constructor(
-					public id: string,
-					public type: HtmlType,
-			) {}
-	}
+  class HtmlElement {
+    constructor(public id: string, public type: HtmlType) {}
+  }
 
-	class InputAttributes   {
-			constructor(
-					public value: string,
-					public placeholder: string,
-			) {
-			}
-	}
+  class InputAttributes {
+    constructor(public value: string, public placeholder: string) {}
+  }
 
-	class InputEvents {
-			constructor() {
-			}
-			setFocus() {};
-			getValue() {};
-			isActive() {};
-			removeValue() {};
-	}
+  class InputEvents {
+    constructor() {}
+    setFocus() {}
+    getValue() {}
+    isActive() {}
+    removeValue() {}
+  }
 
+  //? Idea para la nueva clase InputElement
 
-	//? Idea para la nueva clase InputElement
+  class InputElement {
+    public html: HtmlElement;
+    public attributes: InputAttributes;
+    public events: InputEvents;
+    constructor(value: string, placeholder: string, id: string) {
+      this.html = new HtmlElement(id, "input");
+      this.attributes = new InputAttributes(value, placeholder);
+      this.events = new InputEvents();
+    }
+  }
 
-	class InputElement{
-		public html: HtmlElement;
-		public attributes: InputAttributes;
-		public events: InputEvents;
-		constructor(value: string, placeholder: string, id: string) {
-			this.html = new HtmlElement(id, 'input');
-			this.attributes = new InputAttributes(value, placeholder);
-			this.events = new InputEvents();
-		}
-	}
+  const nameField = new InputElement("Fernando", "Enter first name", "txtName");
 
-	const nameField = new InputElement('Fernando', 'Enter first name', 'txtName');
-
-	console.log({ nameField });
-
-})()
+  console.log({ nameField });
+})();
